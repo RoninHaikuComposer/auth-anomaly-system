35b655925d8af51cc1bdf2cad10502e5de075d53
Akshat Sharma <coalandpetroleum@gmail.com>
2026-05-20 16:21:46 +0530

    added login endpoint with Pydantic validation

--- main.py
+++ main.py
@@ -8,3 +8,19 @@ async def root():
 @app.get("/health")
 async def health_check():
     return{"status":"running"}
+
+
+from pydantic import BaseModel
+
+class LoginRequest (BaseModel):
+    email : str
+    password : str
+
+@app.post("/auth/login")
+async def login(request: LoginRequest):
+    return {
+        "Message":"Login Recieved",
+        "email": request.email
+    }
+
+    
