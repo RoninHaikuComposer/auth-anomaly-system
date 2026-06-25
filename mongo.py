443d7a91339aa2dd03e49672b936a2e747312194
Akshat Sharma <coalandpetroleum@gmail.com>
2026-06-26 01:01:20 +0530

    migrated MongoDB from localhost to Atlas

--- mongo.py
+++ mongo.py
@@ -1,4 +1,10 @@
 from pymongo import MongoClient
-client = MongoClient("mongodb://localhost:27017")
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+client = MongoClient(os.getenv("MONGO_URI"))
+
 db = client["auth_anomaly_db"]
 login_signals = db["login_signals"]
